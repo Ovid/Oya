feb54f0ba9440958ab03e8b9b203b80edaf1c10f
Ovid <curtis_ovid_poe@yahoo.com>
2026-01-14 07:21:10 +0100

    chore: create backend config directory structure

---
+++ backend/src/oya/config/__init__.py
@@ -0,0 +1,7 @@
+"""Configuration constants.
+
+Re-exports all config for convenient importing:
+    from oya.config import MAX_CONTEXT_TOKENS, SYNTHESIS_TEMPERATURE
+"""
+
+# Imports will be added as each config module is created
